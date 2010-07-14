030e25f690700cd3ae0b484d84534f600ce510b8
Carl Olsen <olsen.carl@gmailcom>
2010-07-14 15:52:07 +0200

    Added BSD license. Also removed some unessecary functions

--- qmake.el
+++ qmake.el
@@ -1,3 +1,57 @@
+;; qmake.el -- qmake mode for emacs
+;;
+;; Author: Carl Olsen
+;;
+;; Copyright (c) 2010, carl-olsen
+;; All rights reserved.
+
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions
+;; are met:
+;;
+;;     * Redistributions of source code must retain the above
+;;       copyright notice, this list of conditions and the following
+;;       disclaimer.
+
+;;     * Redistributions in binary form must reproduce the above
+;;       copyright notice, this list of conditions and the following
+;;       disclaimer in the documentation and/or other materials
+;;       provided with the distribution.
+
+;;     * Neither the name of the <ORGANIZATION> nor the names of its
+;;       contributors may be used to endorse or promote products
+;;       derived from this software without specific prior written
+;;       permission.
+
+
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+;; "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+;; LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+;; FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+;; COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+;; INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+;; (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+;; HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+;; STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+;; OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+
+;; -------------------------------------------------------
+;; provides qmake-mode for emacs
+;; qmake is Qt specific make file.
+;; Since there exists no other (what i know of)
+;; qmake mode for emacs, i decided to create on.
+;; If you think this looks messy, its because
+;; Im by know way any hacker when it comes to
+;; lisp and in particular elisp. If you find
+;; any errors or have any good suggestions,
+;; don't hesitate to give me a mail
+;;
+;;-------------------------------------------------------
 (provide 'qmake-mode)
 
 (defvar qmake-mode-hook nil)
@@ -254,11 +308,6 @@
        (if (is-prev-curly-forward)
            (setq new-indent (+ prev-indent qmake-indent-width))
          )
-       ;; If the previous line is a }
-       ;; In that case remove identations width
-;       (if (is-prev-curly-back)
-;         (setq new-indent (- prev-indent qmake-indent-width))
-;         )
        ;; If the current line is a } closing bracket,
        ;; remove identation width
        (if (looking-at "[ \t]*}")
@@ -267,13 +316,13 @@
 
 
        )
-     
+
 
      (if (> 0 new-indent)
          (progn
            (message "Damn %d" new-indent)
            0)
-       (progn 
+       (progn
          (message "indent %d" new-indent)
          new-indent)
        )
@@ -322,22 +371,6 @@
 
 
 
-
-(defun is-prev-curly-back()
-  "Checks if the previous line contains a curly bracket, that is \"}\" "
-  (interactive)
-  (beginning-of-line)
-  (forward-line -1)
-  (if (looking-at "[ \t]*}")
-      (progn
-        (message "curly back")
-        (forward-line 1)
-        t)
-    (progn
-      (forward-line 1)
-      nil)
-    )
-  )
 
 
 
